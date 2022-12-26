b305f3344b2a9bc7f003dbf42c61c4cba3b6bc79
Adesida Leye <adeleyeadesida@gmail.com>
2022-12-26 19:04:52 +0100

    update on files

---
+++ script.js
@@ -0,0 +1,12 @@
+const flyoForm = document.querySelector('form');
+const formInput = document.querySelector('form input');
+const errorMsg = document.querySelector('.error-msg')
+
+
+let inputValue = formInput.value;
+flyoForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    if(inputValue == ''){
+        errorMsg.innerHTML = 'Fill in input field';
+    }
+})
